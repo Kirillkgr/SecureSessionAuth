a5a6a1ad73a505ed369106a3672851e46a26967f
Kirill <RillGd@gmail.com>
2025-08-12 11:13:00 +0200

    Initial commit

---
+++ src/main/java/kirill/zhdanov/fastauthapi/SecureSessionAuthApplication.java
@@ -0,0 +1,13 @@
+package kirill.zhdanov.fastauthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureSessionAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureSessionAuthApplication.class, args);
+    }
+
+}
